a3591a32fdb895f097bd3cbf4d69276ae048776e
TheHolyRoger <theholyroger@theholyroger.com>
2018-05-20 06:42:56 +0100

    genesis

---
+++ confdefs.h
@@ -0,0 +1,31 @@
+/* confdefs.h */
+#define PACKAGE_NAME "The Holy Roger Core"
+#define PACKAGE_TARNAME "the-holy-roger-core"
+#define PACKAGE_VERSION "0.16.0"
+#define PACKAGE_STRING "The Holy Roger Core 0.16.0"
+#define PACKAGE_BUGREPORT "https://github.com/TheHolyRoger/TheHolyRogerCoin/issues"
+#define PACKAGE_URL "https://theholyroger.com/"
+#define HAVE_CXX11 1
+#define STDC_HEADERS 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRING_H 1
+#define HAVE_MEMORY_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_UNISTD_H 1
+#define HAVE_DLFCN_H 1
+#define LT_OBJDIR ".libs/"
+#define USE_ASM 1
+#define HAVE_PTHREAD_PRIO_INHERIT 1
+#define HAVE_PTHREAD 1
+#define HAVE_DECL_STRERROR_R 1
+#define HAVE_STRERROR_R 1
+#define HAVE_STDIO_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_UNISTD_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_SYS_STAT_H 1
