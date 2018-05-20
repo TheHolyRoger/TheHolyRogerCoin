23444586713ef77669c6b2856d22278691c04f68
TheHolyRoger <theholyroger@theholyroger.com>
2018-05-20 05:31:44 +0100

    genesis

--- INSTALL.md
+++ INSTALL.md
@@ -1,5 +1,5 @@
-Building Litecoin
+Building The Holy Roger
 ================
 
 See doc/build-*.md for instructions on building the various
-elements of the Litecoin Core reference implementation of Litecoin.
+elements of the The Holy Roger Core reference implementation of The Holy Roger.
--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -32,7 +32,7 @@ class CBaseMainParams : public CBaseChainParams
 public:
     CBaseMainParams()
     {
-        nRPCPort = 9332;
+        nRPCPort = 9662;
     }
 };
 
@@ -44,7 +44,7 @@ class CBaseTestNetParams : public CBaseChainParams
 public:
     CBaseTestNetParams()
     {
-        nRPCPort = 19332;
+        nRPCPort = 19662;
         strDataDir = "testnet4";
     }
 };
@@ -57,7 +57,7 @@ class CBaseRegTestParams : public CBaseChainParams
 public:
     CBaseRegTestParams()
     {
-        nRPCPort = 19443;
+        nRPCPort = 19773;
         strDataDir = "regtest";
     }
 };
